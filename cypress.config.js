30a3811793ddc6e6300d10beeace770d17476205
Guigoun <guilhermelicati2004@gmail.com>
2026-04-12 17:08:01 -0300

    Estruturando pastas do projeto

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports', 
+      overwrite: false,
+      html: true,
+      json: true,
+      timestamp: 'mmddyyyy_HHMMss'
+    },
+    setupNodeEvents(on, config) {
+      
+    },
+  },
+});
